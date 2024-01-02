9fc977230e473c24f8466ee2b172beb3ba8d2624
kirubason <kirubason12@gmail.com>
2024-01-02 21:34:26 +0530

    Class and Object demo

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -10,6 +10,9 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
+            Mathematics obj = new Mathematics();
+            int Total = obj.Addition(14, 12);
+            Console.WriteLine("The total is " + Total);
         }
     }
 }
