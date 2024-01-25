d9123b1475eaeb1e4dc03d38bf13ce7a48b549df
kirubason <kirubason12@gmail.com>
2024-01-25 10:31:09 +0530

    Exception handling demo

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -10,12 +10,35 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
-            ChildRobot Interfaceobj = new ChildRobot();
-            Interfaceobj.Sensor();
-            Interfaceobj.Body();
-            ChildRobotClass AbstractObj = new ChildRobotClass();
-            AbstractObj.Sensor();
-            AbstractObj.Body();
+            Console.WriteLine(ExceptionMethod());
+        }
+        public static string ExceptionMethod()
+        {
+            int[] ArrInt = new int[3];
+            try
+            {
+                Console.WriteLine("Please enter the index position");
+                int index = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the value");
+                int value = Convert.ToInt32(Console.ReadLine());
+
+                ArrInt[index] = value;
+                return "No exception, The value in the index position " + index + " is " + value;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "There is an exception while calling the method";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "There is an exception while calling the method";
+            }
+            finally
+            {
+                Console.WriteLine("Finally method executed");
+            }
         }
     }
 }
