0743bc1d7ca5f95a23a0bc4c279273729ead649d
kirubason <kirubason12@gmail.com>
2024-09-24 09:42:14 +0530

    Enum demo

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,35 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
-            string CountryName = null;
 
-            string Output = CountryName ?? "India";
+			Console.WriteLine(GetEnumDescription(CollegeRank.AnnaUniversity) + " college councelling code is " + (int)CollegeRank.AnnaUniversity);
+			Console.WriteLine(GetEnumDescription(CollegeRank.GovtClgTvl) + " councelling code is " + (int)CollegeRank.GovtClgTvl);
 
-            Console.WriteLine("The country name is " + Output);
-
-            Console.ReadLine();
+			Console.ReadLine();
 		}
-        
-    }
+
+		public enum CollegeRank
+		{
+			[Description("Anna University")]
+			AnnaUniversity = 1,
+
+			[Description("Government College of Technology, Coimbatore")]
+			GovtClgCoimbatore = 7177,
+
+			[Description("Government College of Engineering, Tirunelveli")]
+			GovtClgTvl = 9508,
+
+			[Description("Mepco Schlenk Engineering College")]
+			MepcoClg = 9517
+		}
+
+		public static string GetEnumDescription(Enum value)
+		{
+			var field = value.GetType().GetField(value.ToString());
+			var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+			return attribute == null ? value.ToString() : attribute.Description;
+		}
+
+
+	}
 }
