0ebb4cc3072fad59351e67ed3e8227effb7e205f
kirubason <kirubason12@gmail.com>
2024-11-03 20:08:18 +0530

    Garbage collection demo

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -11,20 +11,14 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
-            string sentence1 = "My ";
-            sentence1 += "name ";
-            sentence1 += "is ";
-            sentence1 += "Micah Tech";
+			Console.WriteLine("The maximum GC generation is " + GC.MaxGeneration);
 
-            Console.WriteLine(sentence1);
+			Program obj = new Program();
+			Console.WriteLine("The generation of obj is " + GC.GetGeneration(obj));
+			Console.WriteLine("The total allocated memory before GC run is " + GC.GetTotalMemory(false));
 
-            StringBuilder sentence2 = new StringBuilder();
-            sentence2.Append("My ");
-            sentence2.Append("name ");
-            sentence2.Append("is ");
-            sentence2.Append("Micah Tech");
-            Console.WriteLine(sentence2);
-
-        }
+			GC.Collect(0);
+			Console.WriteLine("The total allocated memory after GC run is " + GC.GetTotalMemory(false));
+		}
 	}
 }
