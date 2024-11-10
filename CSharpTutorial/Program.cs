ddf6e51997bcf215c50dff7be4be9871a2837e31
kirubason <kirubason12@gmail.com>
2024-11-10 15:51:18 +0530

    unnecessary removed

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -11,13 +11,6 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
-			using (FileHandler FileHandlerobj = new FileHandler("C:\\Users\\Kirub\\Desktop\\example.txt"))
-			{
-				FileHandlerobj.WriteToFile("Hello, World!");
-			} // Dispose is called automatically at the end of the using block
-
-			FileHandler FileHandlerobj2 = new FileHandler("C:\\Users\\Kirub\\Desktop\\example.txt");
-			GC.Collect(0);
 		}
 
 	}
