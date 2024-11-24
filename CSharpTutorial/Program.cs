266fad90f4229601e12113911d4b7da0289cd165
kirubason <kirubason12@gmail.com>
2024-11-24 12:03:50 +0530

    array demo

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -11,6 +12,13 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
+            string[] months = new string[2];
+			months[0] = "January";
+			months[1] = "February";
+			for (int i = 0; i < months.Length; i++)
+            {
+                Console.WriteLine(months[i]);
+            }
 		}
 
 	}
