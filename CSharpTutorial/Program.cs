53b19b1f7e275bba243233cb5dd28a9f04b0a038
kirubason <kirubason12@gmail.com>
2025-02-08 14:22:38 +0530

    Thread vs Task

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -4,30 +4,53 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpTutorial
 {
 	class Program
-    {
-        static void Main(string[] args)
-        {
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Main method execution is started");
 
-			Election election = new Election("Tirunelveli", 0);
+			// Normal Flow
+			Method1();
+			Method1WithResult();
 
-			election.VotingCountChanged += District.UpdateToDistrictOffice;
-			election.VotingCountChanged += State.UpdateToStateOffice;
-			election.VotingCountChanged += Union.UpdateToUnionOffice;
+			//Thread
+			Thread thread1 = new Thread(Method1);
+			thread1.Start();
 
-			election.VotingCount = 1121;
-			Console.WriteLine();
+			//Task
+			Task<int> task1 = Task.Run(Method1WithResult);
+			int result = task1.Result;
+			Console.WriteLine($"Task result: {result}");
 
-			election.VotingCount = 3450;
-			Console.WriteLine();
-
-			election.VotingCount = 8150;
-			Console.WriteLine();
+			Console.WriteLine("Main method execution is completed");
+			Console.ReadLine();
 		}
-		
+
+		static void Method1()
+		{
+			for (int i = 1; i < 5; i++)
+			{
+				Console.WriteLine($"Thread Method1 execution: {i}");
+				Thread.Sleep(1000); // Simulate work with 1-second delay
+			}
+		}
+		static int Method1WithResult()
+		{
+			int sum = 0;
+			for (int i = 1; i < 5; i++)
+			{
+				Console.WriteLine($"Task MethodWithResult execution: {i}");
+				sum += i;
+				Thread.Sleep(1000); // Simulate work with 1-second delay
+			}
+			return sum;
+
+		}
 	}
 }
